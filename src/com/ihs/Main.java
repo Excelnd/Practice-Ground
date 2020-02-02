bc40ee7901e3fd5e406aaca987f355ef75d63687
Excelnd <ihsanmpm@gmail.com>
2020-02-02 11:48:57 +0530

    DFS Traversal

--- src/com/ihs/Main.java
+++ src/com/ihs/Main.java
@@ -6,17 +6,94 @@ public class Main {
 
 // Print all subarrays of a given array
 
-        int[] ar = {1, 2, 3, 4, 5};
-
-        int array = ar.length;
-
-        for (int starter = 0; starter < array; starter++) {
-            for (int grouper = starter; grouper <= array; grouper++) { // grouping numbers.
-                for (int toScreen = starter; toScreen < grouper; toScreen++) { // print nums.
-                    System.out.print(ar[toScreen] + " ");
-                }
-                System.out.println("");
-            }
-        }
     }
 }
+
+
+/*class DFSInMatrixRecursion {
+
+    public void DFS(int[][] grid) {
+
+        int h = grid.length;
+        if (h == 0)
+            return;
+        int l = grid[0].length;
+
+        //created visited array
+        boolean [][] visited = new boolean[h][l];
+        System.out.println("Depth-First Search: ");
+        DFSUtil(grid, 0,  0, visited);
+    }
+
+    public void DFSUtil(int[][] grid, int row, int col, boolean[][] visited) {
+
+        int H = grid.length;
+        int L = grid[0].length;
+
+        if (row < 0 || col < 0 || row >= H || col >= L || visited[row][col])
+            return;
+
+        //mark the cell visited
+        visited[row][col] = true;
+        System.out.print(grid[row][col] + " ");
+        DFSUtil(grid, row+ 1, col,visited); // go right
+        DFSUtil(grid, row- 1, col,visited); //go left
+        DFSUtil(grid, row, col + 1,visited); //go down
+        DFSUtil(grid, row, col - 1,visited); // go up
+    }
+
+    public static void main(String[] args) {
+
+        int [][] grid = new int[][] {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        DFSInMatrixRecursion d = new DFSInMatrixRecursion();
+        d.DFS(grid);
+    }
+}*/
+
+class MyClass {
+
+    void DFSTravel(int[][] grid) {
+        int h = grid.length;
+        int l = grid[0].length;
+
+        boolean[][] visited = new boolean[h][l];
+
+        DFSdriver(grid, 0, 0, visited);
+    }
+    int count = 0;
+    void DFSdriver(int[][] grid, int row, int col, boolean[][] visited) {
+
+        int H = grid.length;
+        int L = grid[0].length;
+        if (row < 0 || col < 0 || row >= H || col >= L || visited[row][col])
+            return;
+
+        visited[row][col] = true;
+        System.out.println(grid[row][col] + " ");
+
+        DFSdriver (grid, row+1, col, visited);
+        DFSdriver (grid, row, col+1, visited);
+        DFSdriver (grid, row-1, col, visited);
+        DFSdriver (grid, row, col-1, visited);
+    }
+
+    public static void main(String[] args) {
+
+        int [][] grid = new int[][] {
+                {1, 2, 3, 4},
+                {5, 6, 7, 8},
+                {9, 10, 11, 12},
+                {13, 14, 15, 16}
+        };
+        MyClass d = new MyClass();
+        d.DFSTravel(grid);
+    }
+}
+
+
+
